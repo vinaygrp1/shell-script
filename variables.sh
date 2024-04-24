41abfbb0e57e5d10931b2e1ff8482c8e709c7222
vinaygrp1 <vinaygrp1@gmail.com>
2024-04-24 13:37:22 +0530

    devops

--- variables.sh
+++ variables.sh
@@ -1,1 +1,6 @@
-this is agian sample script
+#!/bin/bash
+
+echo "ramesh :: how r u suresh?"
+echo "suresh :: Hi ramesh, i m fine what about you?"
+echo "ramesh :: Iam good, how is your work?
+echo "suresh :: not bad, i am thinking to upgrade to devops"
