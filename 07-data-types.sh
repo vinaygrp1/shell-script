c7fcf0b543ba6ef970d45eee8136b57f017ae52e
vinaygrp1 <vinaygrp1@gmail.com>
2024-04-24 22:58:53 +0530

    hello script

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
- No1=$1
- No2=$2
+
+ NO1=$1
+ NO2=$2
 
 
- SUM=$(($No1+$No2))
+ SUM=$(($NO1+$NO2))
  # + is a operator
- echo "Total of $No1 and $No2 is: $SUM"
+ echo "Total of $NO1 and $NO2 is: $SUM"
