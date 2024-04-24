a532bbc6bb42082bffbe3beca8861c4e43855cdb
vinaygrp1 <vinaygrp1@gmail.com>
2024-04-24 16:29:35 +0530

    hello script

--- 06-array.sh
+++ 06-array.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 MOVIES =("RRR" "DJ TILLU" "LEADER")
 # SIZE OF ABOVE ARRY IS 3
-#index are 0,1,2
+# index are 0,1,2
 # list always starts with 0.
 echo "First Movie is: ${MOVIES[0]}"
-echo "First Movie is: ${MOVIES[0]}"
+echo "First Movie is: ${MOVIES[1]}"
 echo "First Movie is: ${MOVIES[@]}"
