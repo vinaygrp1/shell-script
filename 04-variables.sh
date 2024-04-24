d59baa695bb3d10a7bdfccd6ab2e283c86541fde
vinaygrp1 <vinaygrp1@gmail.com>
2024-04-24 15:16:46 +0530

    hello script

--- 04-variables.sh
+++ 04-variables.sh
@@ -6,7 +6,7 @@ PERSON2=$2
 
 echo "$PERSON1 :: how r u $PERSON2?"
 echo "$PERSON2 :: Hi $PERSON1, i m fine what about you?"
-echo "$PERSON1 :: Iam good, how is your work?
+echo "$PERSON1 :: Iam good, how is your work"?
 echo "$PERSON2 :: I am thinking about devops"
 
  
