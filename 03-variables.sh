0526dd71e46d223490363378e996d8ba2a79fc7b
vinaygrp1 <vinaygrp1@gmail.com>
2024-04-24 15:13:47 +0530

    hello script

---
+++ 03-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# declaring of variable
+PERSON1=vinay # no space between = and value
+PERSON2=lalitha
+# referring of variable
+
+echo "$PERSON1 :: how r u $PERSON2?"
+echo "$PERSON2 :: Hi $PERSON1, i m fine what about you?"
+echo "$PERSON1 :: Iam good, how is your work?
+echo "$PERSON2 :: I am thinking about devops"
+
+ 
---
+++ 04-vaeiables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# declaring of variable
+PERSON1=$1 # no space between = and value
+PERSON2=$2
+# referring of variable
+
+echo "$PERSON1 :: how r u $PERSON2?"
+echo "$PERSON2 :: Hi $PERSON1, i m fine what about you?"
+echo "$PERSON1 :: Iam good, how is your work?
+echo "$PERSON2 :: I am thinking about devops"
+
+ 
